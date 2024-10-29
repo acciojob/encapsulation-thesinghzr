691b747dc4e21f2a714422763247431f9ce2bc25
thesinghzr <yousurajsingh@gmail.com>
2024-10-29 23:21:24 +0530

    1st push

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]){
+
+        RWOnly rwOnly = new RWOnly();
+
+        rwOnly.setName("Suraj");
+
+        //private variable name
+        String name = rwOnly.getName();
+    }
+    //////////////////////////////////////////////
 }
